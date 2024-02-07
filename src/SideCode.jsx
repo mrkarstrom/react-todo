1784f99e2ca10575b9b7aaed33635286c044155b
Magnus Karström <magnus@yesweb.se>
2024-02-08 00:05:01 +0100

    feat (SideCode.jsx): Added a useEffect with dependancy-array of color and count

--- src/SideCode.jsx
+++ src/SideCode.jsx
@@ -2,19 +2,31 @@ import React, { useState, useEffect } from 'react';
 
 function SideCode() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState('green');
 
   useEffect(() => {
     document.title = `Count: ${count}`;
-  });
+  }, [count]);
 
   function addCount() {
     setCount((c) => c + 1);
   }
 
+  function subtractCount() {
+    setCount((c) => c - 1);
+  }
+
+  function changeColor() {
+    setColor((c) => (c === 'green' ? 'red' : 'green'));
+  }
+
   return (
     <>
-      <p>Count: {count}</p>
+      <p style={{ color: color }}>Count: {count}</p>
       <button onClick={addCount}>Add</button>
+      <button onClick={subtractCount}>Subtract</button>
+      <br />
+      <button onClick={changeColor}>Change Color</button>
     </>
   );
 }
