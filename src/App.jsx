91207b16d237fc1f47d7d36b0cca5c1ef271c3ba
Magnus Karström <magnus@yesweb.se>
2024-02-07 23:47:49 +0100

    feat (SideCode.jsx): Added a counter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 // import Intro from './Intro';
-import Todo from './Todo';
+// import Todo from './Todo';
 // import CarList from './CarList';
+import SideCode from './SideCode';
 import './App.css';
 
 function App() {
   return (
     <>
       {/* <CarList /> */}
-      <Todo />
+      {/* <Todo /> */}
       {/* <Intro /> */}
+      <SideCode />
     </>
   );
 }
---
+++ src/SideCode.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+function SideCode() {
+  const [count, setCount] = useState(0);
+
+  function addCount() {
+    setCount((c) => c + 1);
+  }
+
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={addCount}>Add</button>
+    </>
+  );
+}
+
+export default SideCode;
